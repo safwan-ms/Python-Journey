ea0b5146302915513609d7550d79230ec1944830
safwan-ms <safwan.anasms@gmail.com>
2025-06-25 12:19:05 +0530

    Iterating Over List

---
+++ 02. Intermediate/07. Iterating Over List/index.py
@@ -0,0 +1,21 @@
+# ------------------------
+# for loop
+my_list = [1, 2, 3, 4, 5]
+
+for element in my_list:
+    print(element)
+# ------------------------
+
+# ------------------------
+#  Range & Length
+my_list = [1, 2, 3, 4, 5]
+
+for i in range(len(my_list)):
+    print(my_list[i])
+
+# While loop
+my_list = [1, 2, 3, 4, 5]
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
