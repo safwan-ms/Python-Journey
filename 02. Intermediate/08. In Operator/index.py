979f4069703009962f4eedef037a3f9ab990656b
safwan-ms <safwan.anasms@gmail.com>
2025-06-25 12:45:20 +0530

    In Operator

---
+++ 02. Intermediate/08. In Operator/index.py
@@ -0,0 +1,14 @@
+# In Operator
+text = "Hello World"
+result = "o" in text
+print(result)
+
+# In Lists
+numbers = [1, 2, 4, 5]
+result = 1 in numbers
+print(result)
+
+# In Substrings
+sentence = "Python is powerful language"
+result = "Python" in sentence
+print(result)
