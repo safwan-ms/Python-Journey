a4a39f33699467f777830c4ab60c4452b97ad137
safwan-ms <safwan.anasms@gmail.com>
2025-06-27 20:42:43 +0530

    Delete Item From Dictionary

---
+++ 02. Intermediate/03. Dictonaries/04. Delete Items From Dictionary/index.py
@@ -0,0 +1,19 @@
+# del Statement
+my_dict = {"name": "John", "age": 25, "city": "New York"}
+del my_dict["age"]
+print(my_dict)
+
+# pop() Method
+my_dict = {"name": "John", "age": 25, "city": "New York"}
+my_dict.pop("city")
+print(my_dict)
+
+# popitem() Method -> remove last element
+my_dict = {"name": "John", "age": 25, "city": "New York"}
+my_dict.popitem()
+print(my_dict)
+
+# clear() Method
+my_dict = {"name": "John", "age": 25, "city": "New York"}
+my_dict.clear()
+print(my_dict)
