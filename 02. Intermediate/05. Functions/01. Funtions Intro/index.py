6e960c7f0cca6dcdf60b8376cb3df1659fefb01e
safwan-ms <safwan.anasms@gmail.com>
2025-06-28 19:07:17 +0530

    Function Intro

---
+++ 02. Intermediate/05. Functions/01. Funtions Intro/index.py
@@ -0,0 +1,21 @@
+# --------------------------------
+# Example 1
+def greet():
+    print("Hello World")
+
+
+# Calling/Invoking/Executing the function
+greet()
+greet()
+greet()
+# --------------------------------
+
+
+# --------------------------------
+# Example 2
+def print_number():
+    result = 4 + 2
+    print(result)
+
+
+print_number()
