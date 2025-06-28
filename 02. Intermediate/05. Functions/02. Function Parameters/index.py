c3bc4d85462c8a20882ba00efa72be7d1da7887a
safwan-ms <safwan.anasms@gmail.com>
2025-06-28 19:12:14 +0530

    Function Parameters

---
+++ 02. Intermediate/05. Functions/02. Function Parameters/index.py
@@ -0,0 +1,23 @@
+# -----------------------------
+# Example 1
+# This function adds two numbers and prints the result.
+
+
+def print_number(a, b):
+    result = a + b
+    print(result)
+
+
+print_number(2, 4)
+
+# -----------------------------
+
+
+# -----------------------------
+# Example 2
+def username(name, age):
+    print(f"My name is {name}, I'm {age} years old.")
+
+
+username("Safwax", 20)
+# ----------------------------
