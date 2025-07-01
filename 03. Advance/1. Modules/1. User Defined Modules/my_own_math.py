db93a4f5a90579cc05c9ffe5ab5d272ccb59206f
safwan-ms <safwan.anasms@gmail.com>
2025-07-01 17:05:45 +0530

    User Defined Modules

---
+++ 03. Advance/1. Modules/1. User Defined Modules/my_own_math.py
@@ -0,0 +1,21 @@
+my_favorite_num = 7
+
+
+def into():
+    print("This is my own version of math")
+
+
+def add_to_numbers(x, y):
+    return x + y
+
+
+def subtract_to_numbers(x, y):
+    return x - y
+
+
+def multiply_to_numbers(x, y):
+    return x * y
+
+
+def divide_to_numbers(x, y):
+    return x / y
